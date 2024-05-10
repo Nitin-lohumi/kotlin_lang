d589a35602c6088ef197df2f0907e95b34c9625a
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-05-10 17:20:36 +0530

    frist kotlin

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+//    val number:Int=12;
+    var number:Int= 15;
+   do{
+     println(number);
+       number--
+//       if(number==12){
+//           println("break statement is occur");
+//           return;
+//       }
+   }
+       while (number>=10);
+//    println(res);
+}
+
