801e8671bc4aed88dfc3b00a39636c4465bfe1c4
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-09 19:47:40 +0530

    Add

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,18 @@
+data class Employee (val name: String, var salary: Int)
 
-
-fun main() {
-
+fun employeeById(id: Int) = when(id) {
+    1 -> Employee("Mary", 20)
+    2 -> null
+    3 -> Employee("John", 21)
+    4 -> Employee("Ann", 23)
+    else -> null
 }
 
+fun salaryById(id: Int) = employeeById(id)?.salary?: 0
+
+fun main() {
+    println((1..5).sumOf { id -> salaryById(id) })
+}
 //import  kotlin.math.PI;
 //fun toSeconds(time: String): (Int,Int) -> Int = when (time) {
 //    "hour" -> { v1,v2 -> v1 *v2}
